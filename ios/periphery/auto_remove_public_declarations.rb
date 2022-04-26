521ba1791620a1d71ccbd0fd42926760e8be6bce
Mohammad Asif <arshadasif0312@gmail.com>
2022-04-26 11:30:45 +0530

    Add script to remove redundant public warnings from periphery
    includes logic to mark classes as final
    In the future it should be possible to update the script to provide an
    option to choose whether classes should be marked as final or not

---
+++ ios/periphery/auto_remove_public_declarations.rb
@@ -0,0 +1,30 @@
+#!/bin/env/ruby
+require('FileUtils')
+
+input_file = ARGV[0]
+
+File.foreach(input_file) do |line|
+    line_split = line.split(':')
+    file_name = line_split[0].chomp
+    line_number = line_split[1].to_i - 1
+    next unless File.file?(file_name)
+    temp_file = "#{file_name}_modified.swift"
+    output_file = File.open(temp_file, 'w')
+    File.open(file_name, 'r').each_line.with_index do |code_line, line_num|
+        if line_num == line_number
+            if code_line.include?('public')
+                if code_line.include?('public class')
+                    code_line.gsub!(/public /, 'final ')
+                else
+                    code_line.gsub!(/public /, '')
+                end
+                puts(code_line)
+            end
+        end
+        output_file.print(code_line)
+    end
+    output_file.close
+    FileUtils.rm file_name
+    FileUtils.mv temp_file, file_name
+end
+
